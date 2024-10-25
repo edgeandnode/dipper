041cf6af55c14b9a41bd7abdb61d1582ee114d46
Lorenzo Delgado <lorenzo@edgeandnode.com>
2024-10-25 16:06:17 +0200

    chore(deps): remove unused dependencies (#118)

--- crates/bin/dipper-service/src/main.rs
+++ crates/bin/dipper-service/src/main.rs
@@ -1,11 +1,5 @@
 use async_signal::{Signal, Signals};
-use axum::{
-    async_trait, body::Body, extract::FromRequest, http::Request, routing::get, Extension, Router,
-};
-use dipper::{
-    db::DbHandle,
-    models::{Indexer, Key},
-};
+use axum::{routing, Router};
 use futures_lite::StreamExt;
 use log::LevelFilter;
 use thiserror::Error;
@@ -16,26 +10,11 @@ mod config;
 static ALLOC: snmalloc_rs::SnMalloc = snmalloc_rs::SnMalloc;
 
 #[derive(Clone)]
-struct AppState {
-    db: DbHandle,
-}
+struct AppState {}
 
 impl AppState {
-    fn new(db: DbHandle) -> Self {
-        Self { db }
-    }
-}
-
-#[async_trait]
-impl<S> FromRequest<S> for AppState
-where
-    S: Send + Sync,
-{
-    type Rejection = ();
-
-    async fn from_request(req: Request<Body>, _: &S) -> Result<Self, Self::Rejection> {
-        let db = req.extensions().get::<AppState>().unwrap().db.clone();
-        Ok(AppState::new(db))
+    fn new() -> Self {
+        Self {}
     }
 }
 
@@ -47,28 +26,18 @@ async fn main() -> anyhow::Result<()> {
         .init()?;
 
     log::info!("starting dipper-service");
-    let db = DbHandle::load_at(&opts.db_path.unwrap()).await?;
-    let app_state = AppState::new(db);
+
+    let app_state = AppState::new();
     let app = Router::new()
-        .route(
-            "/",
-            get(|extension: Extension<AppState>| async {
-                log::info!("dipper GET /");
-                let Extension(app_state) = extension;
-                let indexers: Option<Vec<Indexer>> =
-                    app_state.db.get(Key::from("indexers")).expect("db error");
-                axum::Json(indexers.unwrap_or_default())
-            }),
-        )
-        .layer(tower::ServiceBuilder::new().layer(Extension(app_state)));
+        .route("/", routing::get(|| async { "OK" }))
+        .with_state(app_state);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:9091").await?;
 
-    let signal_task = async {
-        let _signal = signal_task().await.unwrap();
-    };
     let _app_task = axum::serve(listener, app)
-        .with_graceful_shutdown(signal_task)
+        .with_graceful_shutdown(async {
+            let _signal = signal_task().await.unwrap();
+        })
         .await;
     Ok(())
 }
