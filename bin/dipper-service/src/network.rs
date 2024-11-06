5b0bb6d5bad87ae8dded97e0c6a7ca5ede29a7f5
Lorenzo Delgado <lorenzo@edgeandnode.com>
2024-11-06 18:51:23 +0100

    feat(dipper-service): add graph network module (#139)
    Signed-off-by: Lorenzo Delgado <lorenzo@edgeandnode.com>

--- bin/dipper-service/src/network.rs
+++ bin/dipper-service/src/network.rs
@@ -1,8 +1,11 @@
 //! A service providing information about the indexers in the network.
 
+pub mod api;
+pub mod provider;
 pub mod service;
 mod subgraph;
 
+#[allow(unused_imports)] // TODO: Remove this once the module is used
 pub use subgraph::{
     client::Client as SubgraphClient,
     snapshot::{Deployment, Indexer, Snapshot, Subgraph, SubgraphVersion},
--- integration-tests/tests/integration_tests.rs
+++ integration-tests/tests/integration_tests.rs
@@ -1,7 +1,5 @@
-// Integration tests are special. They rely on local-network being set up and we want to exclude this from the normal flow of `cargo test`.
+// Integration tests are special. They rely on local-network being set up, and we want to exclude this from the normal flow of `cargo test`.
 #![cfg(feature = "integration-tests")]
-
-use dipper::network::NetworkSubgraph;
 
 #[test]
 fn integration_tests_run() {
@@ -11,14 +9,4 @@ fn integration_tests_run() {
 #[tokio::test]
 async fn async_integration_tests_run() {
     assert_eq!(true, true);
-}
-
-#[tokio::test]
-async fn local_network_subgraph_query() {
-    let api_key = "deadbeefdeadbeefdeadbeefdeadbeef";
-    let url = "http://localhost:7700";
-
-    let network_subgraph_client = NetworkSubgraph::new(api_key.to_string(), url.to_string());
-
-    network_subgraph_client.query().await.unwrap();
 }
