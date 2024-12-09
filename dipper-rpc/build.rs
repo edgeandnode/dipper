408200f24c3aaa6130da47d69a8b08b08faa2314
Lorenzo Delgado <lorenzo@edgeandnode.com>
2024-12-09 20:03:18 +0100

    feat(dipper-rpc): split dips.proto into indexer and gateway services
    Signed-off-by: Lorenzo Delgado <lorenzo@edgeandnode.com>

--- dipper-rpc/build.rs
+++ dipper-rpc/build.rs
@@ -1,13 +1,21 @@
 fn main() {
+    // Build the DIPs indexer RPC client
     tonic_build::configure()
-        .include_file("mod.rs")
+        .include_file("indexer.mod.rs")
         .build_client(true)
+        .build_server(false)
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .out_dir("src/indexer/gen")
+        .compile_protos(&["proto/indexer.proto"], &["proto/"])
+        .expect("Failed to compile DIPs indexer RPC proto(s)");
+
+    // Build the DIPs gateway RPC server service
+    tonic_build::configure()
+        .include_file("gateway.mod.rs")
+        .build_client(false)
         .build_server(true)
         .protoc_arg("--experimental_allow_proto3_optional")
         .out_dir("src/indexer/gen")
-        .compile_protos(&["proto/dips.proto"], &["proto"])
-        .expect("Failed to compile dips proto(s)");
-
-    // Instruct cargo to re-run the build script if the proto files change
-    println!("cargo:rerun-if-changed=proto");
+        .compile_protos(&["proto/gateway.proto"], &["proto"])
+        .expect("Failed to compile DIPs gateway RPC proto(s)");
 }
