76ed6d8ae43426f3b82d5296a2ce866844419f8f
Lorenzo Delgado <lorenzo@edgeandnode.com>
2024-12-10 09:03:56 +0100

    feat(dipper-rpc): split dips.proto into indexer and gateway services (#221)
    Signed-off-by: Lorenzo Delgado <lorenzo@edgeandnode.com>

--- dipper-rpc/src/indexer.rs
+++ dipper-rpc/src/indexer.rs
@@ -1,6 +1,21 @@
-//! Indexer RPC API for the DIP gateway.
-mod gen;
+//! DIPs gRPC API for the DIPs Gateway.
+//!
+//! This module contains the generated code to implement the DIPs Gateway's gRPC API:
+//! - [`rpc::gateway_server`]: The tonic gRPC service implementation.
+//! - [`rpc::indexer_client`]: The indexer's DIPs gRPC client.
 
 pub mod rpc {
-    pub use super::gen::*;
+    /// The DIPs gRPC server for the gateway.
+    ///
+    /// This module contains the generated code to implement the gateway's DIPs gRPC server.
+    pub mod gateway_server {
+        include!("indexer/gen/gateway.mod.rs");
+    }
+
+    /// The RPC client for the indexer's DIPs gRPC API.
+    ///
+    /// This module contains the generated code to interact with the indexer's DIPs gRPC server.
+    pub mod indexer_client {
+        include!("indexer/gen/indexer.mod.rs");
+    }
 }
