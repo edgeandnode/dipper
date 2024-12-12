95cf77bb2b1bbe928450eba11c537ac90840c8c7
Lorenzo Delgado <lorenzo@edgeandnode.com>
2024-12-12 13:35:14 +0100

    chore(dipper-rpc): reorg network RPC API modules
    Signed-off-by: Lorenzo Delgado <lorenzo@edgeandnode.com>

---
+++ dipper-rpc/src/indexer/indexer_client.rs
@@ -0,0 +1,6 @@
+//! The RPC client for the indexer's DIPs gRPC API.
+//!
+//! This module contains the generated code to interact with the indexer's DIPs gRPC server.
+
+// Include the tonic-generated code
+include!("gen/indexer.mod.rs");
