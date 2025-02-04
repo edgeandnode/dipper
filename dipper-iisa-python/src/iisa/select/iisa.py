5f3ca0c8f4bfc9c1fb44703eaaca0af26497d5bd
Lorenzo Delgado <lorenzo@edgeandnode.com>
2025-02-04 11:32:23 +0100

    feat(dipper-iisa-python): random indexer selection (#282)
    Signed-off-by: Lorenzo Delgado <lorenzo@edgeandnode.com>

--- dipper-iisa-python/src/iisa/select/iisa.py
+++ dipper-iisa-python/src/iisa/select/iisa.py
@@ -1,47 +1,37 @@
+import random
 from typing import Optional
 
-from ..perf import PerfHistoryDataFrame
-from ..typing import DeploymentId, IndexerId
+from ..typing import IndexerId
 
 
 def select_one(
-    perf: PerfHistoryDataFrame,
-    deployment_id: DeploymentId,
     candidate_pool: list[IndexerId],
-    existing_agreements: Optional[dict[DeploymentId, IndexerId]] = None,
-    rejected_indexers: Optional[dict[DeploymentId, IndexerId]] = None,
 ) -> Optional[IndexerId]:
     """
     Selects a single candidate indexer for indexing a Subgraph deployment.
 
-    :param perf: The curated performance history data.
-    :param deployment_id: The Subgraph deployment ID.
     :param candidate_pool: A list of candidate indexers to select from.
-    :param existing_agreements: A dictionary of existing agreements. Maps deployment IDs to indexer IDs.
-    :param rejected_indexers: A dictionary of rejected indexers. Maps deployment IDs to indexer IDs.
     :return: The selected indexer ID. If no suitable indexer is found, returns None.
     """
-    raise NotImplementedError
+    if len(candidate_pool) == 0:
+        return None
+
+    return random.choice(candidate_pool)
 
 
 def select_many(
-    perf: PerfHistoryDataFrame,
-    deployment_id: DeploymentId,
     candidate_pool: list[IndexerId],
     n: int,
-    existing_agreements: Optional[dict[DeploymentId, IndexerId]] = None,
-    rejected_indexers: Optional[dict[DeploymentId, IndexerId]] = None,
 ) -> list[IndexerId]:
     """
     Selects multiple candidate indexers for indexing a Subgraph deployment.
 
-    :param perf: The curated performance history data.
-    :param deployment_id: The Subgraph deployment ID.
     :param candidate_pool: A list of candidate indexers to select from.
     :param n: The target number of indexers to select.
-    :param existing_agreements: A dictionary of existing agreements. Maps deployment IDs to indexer IDs.
-    :param rejected_indexers: A dictionary of rejected indexers. Maps deployment IDs to indexer IDs.
     :return: A list of selected indexer IDs.
              It can return less than `n` indexers if not enough suitable indexers are found.
     """
-    raise NotImplementedError
+    if n <= 0 or len(candidate_pool) == 0:
+        return []
+
+    return random.choices(candidate_pool, k=min(n, len(candidate_pool)))
