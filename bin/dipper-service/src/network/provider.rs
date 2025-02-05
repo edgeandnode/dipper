b3e35aa17470feb00123b60cdd7e0b54400dba36
Lorenzo Delgado <lorenzo@edgeandnode.com>
2025-02-05 10:46:52 +0100

    feat(dipper-service): filter network provider indexers with allowlist (#286)
    Signed-off-by: Lorenzo Delgado <lorenzo@edgeandnode.com>

--- bin/dipper-service/src/network/provider.rs
+++ bin/dipper-service/src/network/provider.rs
@@ -1,3 +1,5 @@
+use std::collections::BTreeSet;
+
 use thegraph_core::{alloy::primitives::Address, DeploymentId, IndexerId};
 
 use super::{
@@ -9,12 +11,21 @@ use super::{
 pub struct NetworkProviderService {
     /// The network provider service handler.
     inner: Handle,
+    /// The indexers allowlist.
+    ///
+    /// This list contains all the indexers that are allowed to interact with the
+    /// DIPs Gateway. If the indexer is not contained in this list, it must not be
+    /// considered as candidate. If the list is empty, all indexers are allowed.
+    allowlist: BTreeSet<IndexerId>,
 }
 
 impl NetworkProviderService {
     /// Creates a new network provider service instance.
-    pub fn new(inner: Handle) -> Self {
-        Self { inner }
+    pub fn new(inner: Handle, allowlist: impl Into<BTreeSet<IndexerId>>) -> Self {
+        Self {
+            inner,
+            allowlist: allowlist.into(),
+        }
     }
 }
 
@@ -23,13 +34,15 @@ impl NetworkProvider for NetworkProviderService {
         self.inner
             .snapshot()
             .get_deployment(deployment_id)
-            .map(|_deployment| Deployment {})
+            .map(|_| Deployment {})
     }
 
     fn get_indexer_by_id(&self, indexer_id: &IndexerId) -> Option<Indexer> {
         self.inner
             .snapshot()
             .get_indexer(indexer_id)
+            // Filter out indexers that are not in the allowlist
+            .filter(|indexer| self.allowlist.is_empty() || self.allowlist.contains(&indexer.id))
             .map(|indexer| Indexer {
                 id: indexer.id,
                 url: indexer.url.clone(),
@@ -43,6 +56,9 @@ impl NetworkProvider for NetworkProviderService {
         self.inner
             .snapshot()
             .indexers_iter()
+            // Filter out indexers that are not in the allowlist
+            .filter(|indexer| self.allowlist.is_empty() || self.allowlist.contains(&indexer.id))
+            // Filter out indexers that are already indexing the deployment
             .filter(|indexer| !indexer.indexings.contains(deployment_id))
             .map(|indexer| Indexer {
                 id: indexer.id,
